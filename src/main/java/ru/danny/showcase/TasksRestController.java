b400efd87c2f965e2b0e92806522c5791dedc74c
cherrydan <cherrydan.1978@gmail.com>
2023-08-04 14:10:00 +0300

    Add POST query error handling with messaging

--- src/main/java/ru/danny/showcase/TasksRestController.java
+++ src/main/java/ru/danny/showcase/TasksRestController.java
@@ -1,12 +1,15 @@
 package ru.danny.showcase;
 
+import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("api/tasks")
@@ -14,8 +17,12 @@ public class TasksRestController {
 
     private final TaskRepository taskRepository;
 
-    public TasksRestController(TaskRepository taskRepository) {
+    private final MessageSource messageSource;
+
+    public TasksRestController(TaskRepository taskRepository, MessageSource messageSource) {
         this.taskRepository = taskRepository;
+        this.messageSource = messageSource;
+
     }
 
     @GetMapping
@@ -25,13 +32,24 @@ public class TasksRestController {
     }
 
     @PostMapping
-    public ResponseEntity<Task> handleCreateNewTask(@RequestBody NewTaskPayload payload,
-                                                    UriComponentsBuilder uriComponentsBuilder) {
-        var task = new Task(payload.details());
-        this.taskRepository.save(task);
-        return ResponseEntity.created(uriComponentsBuilder.path("/api/taks/{taskId}")
-                .build(Map.of("taskId", task.id()))).contentType(MediaType.APPLICATION_JSON).body(task);
+    public ResponseEntity<?> handleCreateNewTask(@RequestBody NewTaskPayload payload,
+                                                    UriComponentsBuilder uriComponentsBuilder,
+                                                 Locale locale) {
 
+        var message = this.messageSource.getMessage("tasks.create.details.errors.not_set",
+                new Objects[0], locale);
+        if (payload.details() == null || payload.details().isBlank()) {
+
+            return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).
+                    body(new ErrorsPresentation(List.of(message))); //placeholder
+
+        } else {
+            var task = new Task(payload.details());
+            this.taskRepository.save(task);
+            return ResponseEntity.created(uriComponentsBuilder.path("/api/taks/{taskId}")
+                    .build(Map.of("taskId", task.id()))).contentType(MediaType.APPLICATION_JSON).body(task);
+
+        }
     }
 
 }
